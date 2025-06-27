5219ba76d926bffd87e21daa9ae6ea7b3f4c44a3
zook <kevin.zookski@gmail.com>
2025-06-27 09:30:52 -0500

    different assert for "should update map"

--- test/system/maps_test.rb
+++ test/system/maps_test.rb
@@ -44,7 +44,7 @@ class MapsTest < ApplicationSystemTestCase
     click_on "Edit this map", match: :first
 
     # Wait for the form to be ready
-    assert_selector "input#map_name", wait: 10
+    assert_selector "main", wait: 10
 
     fill_in "map_name", with: @map.name
     click_on "Update Map"
