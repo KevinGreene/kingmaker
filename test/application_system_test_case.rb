c0aefa8bdb2ad212f071ce6689ddd5bd366d3759
zook <kevin.zookski@gmail.com>
2025-07-03 15:17:24 -0500

    rubocop

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,10 +3,10 @@ require_relative "helpers/login"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_options|
-    driver_options.add_argument('--no-sandbox')
-    driver_options.add_argument('--disable-dev-shm-usage')
-    driver_options.add_argument('--disable-gpu')
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver_options.add_argument("--disable-gpu")
     # This might help with confirm dialogs:
-    driver_options.add_argument('--enable-features=VizDisplayCompositor')
+    driver_options.add_argument("--enable-features=VizDisplayCompositor")
   end
 end
